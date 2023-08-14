7f9a8f9c03b5cfaf5c3b0f53f364433499aa7294
Jieun011211 <qhfltldkt@naver.com>
2023-08-14 12:42:40 +0900

    배치프로그램 추가

--- src/main/java/org/koreait/schedule/BoardStat.java
+++ src/main/java/org/koreait/schedule/BoardStat.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BoardStat {
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "0 0 1 * * *")
     public void makeStat() {
-        System.out.println("5초마다 실행");
+        System.out.println("매일 새벽 1시 정각마다 실행");
     }
 }
